2721f31d4d855470d65781f2b76bd0bdb2a5bf0c
elife <mert_elife@hotmail.com>
2022-01-31 18:04:23 -0500

    first commit

---
+++ src/com/works/Main.java
@@ -0,0 +1,8 @@
+package com.works;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/works/class01/gitLocation.java
@@ -0,0 +1,10 @@
+package com.works.class01;
+
+public class gitLocation {
+    public static void main(String[] args) {
+        int i = 5;
+        String name = "sinan";
+        System.out.println(name+" " +i );
+
+    }
+}
